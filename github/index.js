4e647a7f3cb43cef001e0e87e35e1b4eb876552c
James Ma <40496186+kuwas@users.noreply.github.com>
2018-11-28 10:57:21 -0500

    fix(github): disable semantic release github push

--- github/index.js
+++ github/index.js
@@ -1,25 +1,27 @@
 module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+  /*
   verifyConditions: [
     '@semantic-release/github'
   ],
-
   analyzeCommits: [
     '@semantic-release/commit-analyzer'
   ],
-
   generateNotes: [
     '@semantic-release/release-notes-generator'
   ],
-
   publish: [
     '@semantic-release/github'
   ],
-
   success: [
     '@semantic-release/github'
   ],
-
   fail: [
     '@semantic-release/github'
   ]
+  */
 }
