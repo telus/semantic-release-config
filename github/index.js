bbdb84d0f30a49fe75993bfc41f0f22d8ae1553c
James Ma <40496186+kuwas@users.noreply.github.com>
2018-11-28 11:21:34 -0500

    chore: removed github commented out lines

--- github/index.js
+++ github/index.js
@@ -4,24 +4,4 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/github'
   ]
-  /*
-  verifyConditions: [
-    '@semantic-release/github'
-  ],
-  analyzeCommits: [
-    '@semantic-release/commit-analyzer'
-  ],
-  generateNotes: [
-    '@semantic-release/release-notes-generator'
-  ],
-  publish: [
-    '@semantic-release/github'
-  ],
-  success: [
-    '@semantic-release/github'
-  ],
-  fail: [
-    '@semantic-release/github'
-  ]
-  */
 }
