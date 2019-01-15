d707ed10a571c55f67298a979c789a86ae75aad2
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2019-01-15 12:08:41 -0800

    feat(commit analyzer): update settings for major release detection
    ensure both `BREAKING CHANGE` and `BREAKING CHANGES` detect a major release

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ module.exports = {
   ],
 
   analyzeCommits: [
-    '@semantic-release/commit-analyzer'
+    '@semantic-release/commit-analyzer',
+    {
+      'preset': 'angular',
+      'parserOpts': {
+        'noteKeywords': ['BREAKING CHANGE', 'BREAKING CHANGES']
+    }
   ],
 
   generateNotes: [
