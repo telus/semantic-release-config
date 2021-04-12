0e2fad70aeea219b0cbadb854e95f561ab32fd78
Nitin Agrawal <nitin.agrawal1@telusinternational.com>
2021-04-12 22:22:38 +0530

    fix: gha and outdated dep

--- github/index.js
+++ github/index.js
@@ -2,9 +2,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
-    ['@semantic-release/git', {
-      assets: ['package.json']
-    }]
+    '@semantic-release/github'
   ]
 }
